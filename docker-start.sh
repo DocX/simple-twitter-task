06b441fd28cd68f7dd1fd5991a9d39c3bf2fac91
Lukas Dolezal <lukas@dolezalu.cz>
2019-07-25 16:36:32 +0200

    fix docker-start script

--- docker-start.sh
+++ docker-start.sh
@@ -4,6 +4,6 @@ export NODE_ENV=production
 export PORT=4000
 
 yarn install && 
-./node_modules/.bin/sequelize db:create --config config/database.json &&
+( ./node_modules/.bin/sequelize db:create --config config/database.json || true ) &&
 ./node_modules/.bin/sequelize db:migrate --config config/database.json &&
 yarn run start
